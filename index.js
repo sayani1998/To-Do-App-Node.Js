bd1c65093064c4b781ed75e3774cfd03ed2b1bdd
Sayani Manna <sayanimanna0@gmail.com>
2023-01-13 14:43:33 +0530

    My To Do App

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+//set up express server
 const express = require('express');
 const port = 8000;
+const db = require('./config/mongoose');
+const Todo = require('./models/todo_task');
 const app = express();
+app.use(express.urlencoded());
+app.use('/',require('./routes'));
 
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.static('assets'));
+
+//delete by id
+app.get('/delete-contact',function(req,res){
+    const id = req.query.id;
+    Todo.findByIdAndDelete(id,function(err){
+      if(err){
+        console.log('error for deleting data from database');
+        return;
+      }
+      return res.redirect('back');
+
+    });
+   });
 
 
 
